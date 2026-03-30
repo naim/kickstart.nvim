b125d42bd9fa069fee96637298589f2b419d5331
Naim Falandino <92351+naim@users.noreply.github.com>
2026-03-30 12:19:24 -0400

    Add lualine and DAP

--- init.lua
+++ init.lua
@@ -383,6 +383,62 @@ require('lazy').setup({
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
+  { -- Collection of various small independent plugins/modules
+    'nvim-mini/mini.nvim',
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
+
+      -- Simple and easy statusline.
+      --  You could remove this setup call if you don't like it,
+      --  and try some other statusline plugin
+      local statusline = require 'mini.statusline'
+      -- set use_icons to true if you have a Nerd Font
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+
+      -- Icon support
+      require('mini.icons').setup()
+      MiniIcons.mock_nvim_web_devicons()
+
+      -- Tabline for open buffers
+      require('mini.tabline').setup {
+        tabpage_section = 'right',
+      }
+
+      -- Animated indent scope visualization
+      require('mini.indentscope').setup {
+        draw = { delay = 0, animation = require('mini.indentscope').gen_animation.none() },
+        options = { indent_at_cursor = false },
+      }
+
+      -- More capabilities for diff hunks
+      require('mini.diff').setup()
+
+      -- ... and there is more!
+      --  Check out: https://github.com/nvim-mini/mini.nvim
+    end,
+  },
+
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
@@ -1019,61 +1075,6 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
-  { -- Collection of various small independent plugins/modules
-    'nvim-mini/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-
-      -- Icon support
-      require('mini.icons').setup()
-      MiniIcons.mock_nvim_web_devicons()
-
-      -- Tabline for open buffers
-      require('mini.tabline').setup {
-        tabpage_section = 'right',
-      }
-
-      -- Animated indent scope visualization
-      require('mini.indentscope').setup {
-        draw = { delay = 0, animation = require('mini.indentscope').gen_animation.none() },
-        options = { indent_at_cursor = false },
-      }
-
-      -- More capabilities for diff hunks
-      require('mini.diff').setup()
-
-      -- ... and there is more!
-      --  Check out: https://github.com/nvim-mini/mini.nvim
-    end,
-  },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
@@ -1202,6 +1203,15 @@ require('lazy').setup({
     keys = {
       { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
     },
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-mini/mini.icons' },
+    config = function()
+      MiniIcons.mock_nvim_web_devicons()
+      require('lualine').setup {}
+    end,
   },
 
   { -- Multi-file search and replace
@@ -1343,6 +1353,11 @@ require('lazy').setup({
       vim.opt.foldlevel = 99
       vim.opt.foldlevelstart = 99
     end,
+  },
+
+  { -- Debugging support
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
   },
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
