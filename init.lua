2337c84b7171695f93f6ef50869e2547a1c0d985
Naim Falandino <92351+naim@users.noreply.github.com>
2026-04-08 10:38:15 -0400

    Disable hererocks to clean up :checkhealth

--- init.lua
+++ init.lua
@@ -1371,6 +1371,10 @@ require('lazy').setup({
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
+  rocks = {
+    -- Disable since it's not being used to clean up :checkhealth
+    hererocks = false,
+  },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
