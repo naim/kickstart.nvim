03e1dc371f1f180b6fcd523237c7378774c9d81d
Naim Falandino <92351+naim@users.noreply.github.com>
2026-04-15 14:19:43 -0400

    Change toggle keybinding for flash

--- init.lua
+++ init.lua
@@ -1207,7 +1207,7 @@ require('lazy').setup({
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      { "<leader>tl", mode = { "n", "x", "o" }, function() require("flash").toggle() end, desc = "[T]oggle F[l]ash Search" },
     },
   },
 
