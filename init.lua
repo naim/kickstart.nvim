3f692f40c0c4d2abe44e31b241276e563e7ce6ed
Naim Falandino <92351+naim@users.noreply.github.com>
2026-04-23 21:52:06 -0400

    Add edgy and trouble

--- init.lua
+++ init.lua
@@ -893,7 +893,7 @@ require('lazy').setup({
         end
       end,
       formatters_by_ft = {
-        json = { 'jq' },
+        json = { 'prettierd', 'jq', stop_after_first = true },
         lua = { 'stylua' },
         python = { 'isort', 'ruff' },
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
@@ -1207,7 +1207,7 @@ require('lazy').setup({
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<leader>tl", mode = { "n", "x", "o" }, function() require("flash").toggle() end, desc = "[T]oggle F[l]ash Search" },
+      { "<leader>tl", mode = { "n", "x" }, function() require("flash").toggle() end, desc = "[T]oggle F[l]ash Search" },
     },
   },
 
@@ -1222,6 +1222,50 @@ require('lazy').setup({
       vim.keymap.set('n', '<Leader>gl', ':Git log<CR>', { noremap = true, desc = 'Git Log' })
       vim.keymap.set('n', '<Leader>gd', ':Gdiff<CR>', { noremap = true, desc = 'Git Diff' })
     end,
+  },
+
+  { -- Predefined layout management
+    'folke/edgy.nvim',
+    event = 'VeryLazy',
+    opts = {},
+  },
+
+  { -- Diagnostic, reference, telescope result, quickfix, and location list
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
   },
 
   { -- Oil filesystem browser
